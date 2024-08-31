5c30218895e9483c13a104876e7bfd922fb45d3c
Dávid István Bíró <noise64@gmail.com>
2024-08-31 11:04:34 +0200

    rpc calls

--- src/components/component-one/main.ts
+++ src/components/component-one/main.ts
@@ -1,13 +1,21 @@
 import {ComponentOneApi} from "./generated/component-one";
 import {ComponentTwoApi} from "golem:component-two-stub/stub-component-two";
+import {ComponentThreeApi} from "golem:component-three-stub/stub-component-three";
 
 let state = BigInt(0);
 
 export const componentOneApi: ComponentOneApi = {
     add(value: bigint) {
         console.log(`Adding ${value} to the counter`);
+
+        console.log("Calling component two");
         const componentTwo = new ComponentTwoApi({value: "urn"});
         componentTwo.blockingAdd(value);
+
+        console.log("Calling component three");
+        const componentThree = new ComponentThreeApi({value: "urn"});
+        componentThree.blockingAdd(value);
+
         state += value;
     },
     get() {
--- src/components/component-two/main.ts
+++ src/components/component-two/main.ts
@@ -1,10 +1,16 @@
 import {ComponentTwoApi} from "./generated/component-two";
+import {ComponentThreeApi} from "golem:component-three-stub/stub-component-three";
 
 let state = BigInt(0);
 
 export const componentTwoApi: ComponentTwoApi = {
     add(value: bigint) {
         console.log(`Adding ${value} to the counter`);
+
+        console.log("Calling component three");
+        const componentThree = new ComponentThreeApi({value: "urn"});
+        componentThree.blockingAdd(value);
+
         state += value;
     },
     get() {
